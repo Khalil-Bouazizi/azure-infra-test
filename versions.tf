8c7eac471a95a413e6103ec406efe3aa92c59c12
Khalil <khalil.bouazizi@esprit.tn>
2026-03-24 11:22:09 +0100

    add azure backend config

--- versions.tf
+++ versions.tf
@@ -1,7 +1,13 @@
 terraform {
   required_version = ">= 1.14.0"
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-demo-hubspoke"
+    storage_account_name = "demoitfstate"
+    container_name       = "tfstate"
+    key                  = "hubspoke.terraform.tfstate"
+    use_azuread_auth     = true
+  }
 
   required_providers {
     azurerm = {
